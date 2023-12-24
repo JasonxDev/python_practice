888932aed6d5645213d4cca423cf7ed9e0f25ef6
JasonxDev <jasonxdev@gmail.com>
2023-12-23 23:53:39 -0500

    Multiple Except blocks

--- first.py
+++ first.py
@@ -1,1 +1,14 @@
-int("This is not a number")
+def divide(a, b):
+    try:
+        int("This is not a number")  # CAUSES VALUE ERROR
+        return a / b
+
+    except ValueError as e:
+        print("ValueError", e)
+    except NameError as e:
+        print("NameError", e)
+    except Exception as e:  # should go last, or else other except blocks will be unreachable
+        print("General", e)
+
+
+divide(9, 0)
