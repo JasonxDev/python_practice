71c91dce4cac1f5c1d5f287a1db4c29673669ddd
JasonxDev <jasonxdev@gmail.com>
2023-12-25 21:24:53 -0500

    Save details about poetry installation

--- first.py
+++ first.py
@@ -1,20 +1,22 @@
-from datetime import datetime, timezone
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
+today = date.today()  # Date object for 2023-12-25
+twoMonthsLater = today + relativedelta(months=2)  # Date object for 2024-02-25
 
-def create_utc_datetime(year, month, day, hour=0, minute=0, second=0, millisecond=0):
-    # Create a datetime object with the provided parameters
-    dt = datetime(year, month, day, hour, minute, second, millisecond * 1000)
+# Now convert the date object to a string
+dateString = twoMonthsLater.strftime("%m-%d-%Y")  # 02-25-2024
+print(dateString)
 
-    # Assuming the provided datetime is in local time, convert it to UTC
-    # Note: This assumes your local time is the system's current timezone
-    dt_utc = dt.astimezone(timezone.utc)
+"""
+Poetry (1.7.1) is installed now. Great!
 
-    # Format the datetime in the specified format
-    formatted_utc_datetime = dt_utc.strftime(
-        "%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"  # Trim to milliseconds and add 'Z'
-    return formatted_utc_datetime
+To get started you need Poetry's bin directory (C:\Users\jason\AppData\Roaming\Python\Scripts) in your `PATH`
+environment variable.
 
+Alternatively, you can call Poetry explicitly with `C:\Users\jason\AppData\Roaming\Python\Scripts\poetry`.
 
-# Example usage
-print(create_utc_datetime(2023, 12, 25))  # 2023-12-25T05:00:00.000Z
-print(create_utc_datetime(2023, 8, 7))  # 2023-08-07T04:00:00.000Z
+You can test that everything is set up by executing:
+
+`poetry --version`
+"""
