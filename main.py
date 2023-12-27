1af195db1810f1a7690cb71d14aa1be18a795a35
JasonxDev <jasonxdev@gmail.com>
2023-12-27 12:54:22 -0500

    Poetry success text

--- main.py
+++ main.py
@@ -8,15 +8,3 @@ twoMonthsLater = today + relativedelta(months=2)  # Date object for 2024-02-25
 dateString = twoMonthsLater.strftime("%m-%d-%Y")  # 02-25-2024
 print(dateString)
 
-# """
-# Poetry (1.7.1) is installed now. Great!
-
-# To get started you need Poetry's bin directory (C:\Users\jason\AppData\Roaming\Python\Scripts) in your `PATH`
-# environment variable.
-
-# Alternatively, you can call Poetry explicitly with `C:\Users\jason\AppData\Roaming\Python\Scripts\poetry`.
-
-# You can test that everything is set up by executing:
-
-# `poetry --version`
-# """
