e367c122ef176140a555b413e6e11ea51467fd9a
JasonxDev <jasonxdev@gmail.com>
2023-12-27 14:52:09 -0500

    Day diff test 1

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from datetime import date
 from dateutil.relativedelta import relativedelta
 
-today = date.today()  # Date object for 2023-12-25
-twoMonthsLater = today + relativedelta(months=2)  # Date object for 2024-02-25
-
+# Take object for today's date and fast forward 2 months using dateutil library
+today = date.today()  # Date object for 2023-12-27
+yesterday = today + relativedelta(days=20)  # Date object for 2023-12-26
+print(today)
 # Now convert the date object to a string
-dateString = twoMonthsLater.strftime("%m-%d-%Y")  # 02-25-2024
-print(dateString)
+dateString = yesterday.strftime("%Y-%m-%d")  # 2023-12-26
+print(yesterday)
 
+diff = abs(today - yesterday)
+print(diff)
